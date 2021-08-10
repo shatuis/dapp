b7b51c7e0f07eb3238d0a71835105e76efcaaaf6
shatuis <inggomezjorge1@gmail.com>
2021-08-11 00:01:00 +0200

    rafa callate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template>
+    <section>
+        <b-field>
+            <b-checkbox>Basic</b-checkbox>
+        </b-field>
+        <b-field>
+            <b-checkbox v-model="checkbox">
+                {{ checkbox }}
+            </b-checkbox>
+        </b-field>
+        <b-field>
+            <b-checkbox v-model="checkboxCustom"
+                true-value="Yes"
+                false-value="No">
+                {{ checkboxCustom }}
+            </b-checkbox>
+        </b-field>
+        <b-field>
+            <b-checkbox :indeterminate="true">
+                Indeterminate
+            </b-checkbox>
+        </b-field>
+        <b-field>
+            <b-checkbox disabled>Disabled</b-checkbox>
+        </b-field>
+    </section>
+</template>
   </div>
 </template>
 
@@ -15,7 +40,16 @@ export default {
   }
 }
 </script>
-
+<script>
+    export default {
+        data() {
+            return {
+                checkbox: false,
+                checkboxCustom: 'Yes'
+            }
+        }
+    }
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
