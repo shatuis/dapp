fe530f449dc7ab305d8993ae6ec65fdd102201a9
shatuis <inggomezjorge1@gmail.com>
2021-08-10 22:58:39 +0200

    ahora si rafa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,24 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <section>
+        <div>
+          <template>
+            <section>
+                <b-button @click="clickMe">Click Me</b-button>
+            </section>
+        </template>
+        <script>
+            export default {
+                methods: {
+                    clickMe() {
+                        this.$buefy.notification.open('Clicked!!')
+                    }
+                }
+            }
+        </script>
+        </div>
+      </section>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
