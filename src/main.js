c2f92166852cb14fc09b48971057a80b074a0453
shatuis <inggomezjorge1@gmail.com>
2021-08-10 22:54:11 +0200

    rafa esto funcionara

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
 
 createApp(App).mount('#app')
